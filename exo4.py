3305e71714e2ac3182d2dcaf8e504fc420d61dab
Nesrine didi <n.didi@it-students.fr>
2023-11-21 16:40:05 +0100

    modification exo python

--- exo4.py
+++ exo4.py
@@ -3,3 +3,9 @@ result = 1
 for i in range(1,n+1) :
     result= result*i 
 print(result)    
+
+entree = 5
+for var in range(entree - 1):
+    var = var + 1
+    entree = var * entree
+print(entree)    
